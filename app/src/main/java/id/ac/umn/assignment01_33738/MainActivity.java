4b7a25c984da89e72471dc40904db37527740edd
Michael Krisna Cahyadi <cahyadi.michael.mkc24@gmail.com>
2021-09-09 20:41:39 +0700

    finish calculator

---
+++ app/src/main/java/id/ac/umn/assignment01_33738/MainActivity.java
@@ -0,0 +1,13 @@
+package id.ac.umn.assignment01_33738;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
